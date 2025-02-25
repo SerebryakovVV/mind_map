2a4197fdb0401760e3d71ea0d5a6601a7f7e2f3b
SerebryakovVV <serebryakov.valentin@gmail.com>
2025-02-26 02:45:53 +0700

    multiline string test

--- main.c
+++ main.c
@@ -8,9 +8,24 @@ typedef struct Node {
     int width;
 } Node;
 
+
+// get the strings from db
+// get the width of a letter in a font
 void drawMultilineString(char* wholeString, Node node) {
     // get the length of the string in pixels or something
-    DrawText(wholeString, node.x, node.y, 10, BLACK);
+    int l = MeasureText(wholeString, 10);
+
+    // also pass the length of the string
+    // insert the termination, then change back
+    if (l > node.width) {
+        char temp = wholeString[20];
+        wholeString[20] = '\0';
+        DrawText(wholeString, node.x, node.y, 10, BLACK);
+        wholeString[20] = temp;
+        DrawText((char*)(wholeString+20), node.x, node.y+30, 10, BLACK); // it is even working somehow
+    } else {
+        DrawText(wholeString, node.x, node.y, 10, BLACK);
+    }
 }
 
 int main() {
@@ -28,14 +43,18 @@ int main() {
     char taskStringLong[] = "this is a task and it is somewhat long";
     char taskStringVeryLong[] = "this is a task and it is somewhat long, actually it is very long";
 
+    int l = MeasureText(taskStringVeryLong, 10);
+    printf("%d", l);
+
     while (!WindowShouldClose()) {
       
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawRectangle(task.x, task.y, task.width, task.height, BLUE);
-        DrawText(taskStringLong, 10, 10, 10, BLACK);
-        drawMultilineString(taskStringShort, task);
+        // DrawText(taskStringLong, 10, 10, 10, BLACK);
+        // drawMultilineString(taskStringShort, task);
+        drawMultilineString(taskStringVeryLong, task);
         
 
         EndDrawing();
